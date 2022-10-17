97dbda3c0051fac844bff21bd0c7c33429e57b05
asdpn <adrianasdpn@gmail.com>
2022-10-17 17:27:31 -0300

    Botando CORS como https

--- src/main/java/com/adriana/GerenciadorTreinamentos/GerenciadorTreinamentosApplication.java
+++ src/main/java/com/adriana/GerenciadorTreinamentos/GerenciadorTreinamentosApplication.java
@@ -20,7 +20,7 @@ public class GerenciadorTreinamentosApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://gerenciador-treinamentos.herokuapp.com/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://gerenciador-treinamentos.herokuapp.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Resquest-Method", "Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
