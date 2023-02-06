87f21a3be06fae0b3b7026f11f23cb6c591fc383
asdpn <adrianasdpn@gmail.com>
2023-02-06 09:00:30 -0300

    Adicionado CORS para o site heroku

--- src/main/java/com/adriana/GerenciadorTreinamentos/config/WebConfig.java
+++ src/main/java/com/adriana/GerenciadorTreinamentos/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig
 			return new WebMvcConfigurer() {
 				@Override
 				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("http://localhost:4200");
+					registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("http://localhost:4200","https://gerenciador-treinamentos.herokuapp.com/");
 				}
 		};
 	}
