9b998754c0e3690efc23de28447ea6b3016dc6bc
asdpn <adrianasdpn@gmail.com>
2023-02-07 07:50:24 -0300

    Rebuildando e reiniciando a base para heroku

--- src/main/java/com/adriana/GerenciadorTreinamentos/config/WebConfig.java
+++ src/main/java/com/adriana/GerenciadorTreinamentos/config/WebConfig.java
@@ -1,21 +1,15 @@
 package com.adriana.GerenciadorTreinamentos.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig
-{
-	@Bean
-		public WebMvcConfigurer corsConfigurer()
-		{
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("https://gerenciador-treinamentos.herokuapp.com");
-				}
-		};
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("http://localhost:4200","https://gerenciador-treinamentos.herokuapp.com");
 	}
 }
